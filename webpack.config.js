fb44a1f7aa5aa4239a905fbe02254d6b32c70ced
Tim <maxim_4542@mail.ru>
2023-06-15 14:30:54 +0300

    server settings

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
+			inject: "body",
     }),
 		new CleanWebpackPlugin(),
 		new MiniCssExtractPlugin(),
